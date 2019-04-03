d3da3c5fa2749466797b117438822802bcac036c
Andrew Kuczynski <kuczynskiandrew@gmail.com>
2019-04-03 12:09:34 -0400

    minor search page update

---
+++ JS/search.js
@@ -0,0 +1,12 @@
+$( function() {
+  $( "ul.droptrue" ).sortable({
+    connectWith: "ul"
+  });
+
+  $( "ul.dropfalse" ).sortable({
+    connectWith: "ul",
+    dropOnEmpty: false
+  });
+
+  $( "#priorities, #nopreference" ).disableSelection();
+} );
