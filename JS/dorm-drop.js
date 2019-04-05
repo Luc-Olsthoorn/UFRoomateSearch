7861011f7cd527a03e81101edee4e2a8c90b5b46
Luc Olsthoorn <lucols97@gmail.com>
2019-04-05 08:49:56 -0400

    fixed some dropdowns

--- JS/dorm-drop.js
+++ JS/dorm-drop.js
@@ -1,9 +1,59 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
+    var instances = M.FormSelect.init(elems, []);
+});
+const buildings ={
+  beaty:[ 
+    {val:"beaty", option:"Beaty Towers"}
+  ],
+  broward:[ 
+    {val:"broward", option:"Broward Hall"},
+    {val:"rawlings", option:"Rawlings Hall"}
+  ],
+  murphree:[ 
+    {val:"buckman", option:"Buckman Hall"},
+    {val:"fletcher", option:"Fletcher Hall"},
+    {val:"murphree", option:"Murphree Hall"},
+    {val:"sledd", option:"Sledd Hall"},
+    {val:"thomas", option:"Thomas Hall"}
+  ],
+  yulee:[ 
+    {val:"buckman", option:"Buckman Hall"},
+    {val:"fletcher", option:"Fletcher Hall"},
+    {val:"murphree", option:"Murphree Hall"},
+    {val:"sledd", option:"Sledd Hall"},
+    {val:"thomas", option:"Thomas Hall"}
+  ]
+}
+
+
+$('#area').on('change', (e) => {
+  onAreaChange(e.target.value);
 });
 
-document.getElementById("area-select").addEventListener("change", areaSelect());
+const onAreaChange = (val) =>{
+  if(buildings[val]){
+    //actually run the rebuild
+    $('#building').empty();
+    let output = `<div class="input-field col s12">
+        <select id="building-select" onchange="">`;
+    buildings[val].forEach((element)=>{
+      console.log(element);
+        output += `<option value="${element.val}"  >${element.option}</option>`;
+        
+    });
+    output += `/select>
+        </div>`;
+    $('#building').append(output);
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, []);
+  }else{
+    //just empty it
+    $('#building').empty();
+  }
+}
+onAreaChange("beaty");
+//document.getElementById("area-select").addEventListener("change", areaSelect());
 /*
   var select = document.getElementById("selectNumber");
   var options = ["1", "2", "3", "4", "5"];
@@ -16,6 +66,8 @@ document.getElementById("area-select").addEventListener("change", areaSelect());
       select.appendChild(el);
   }​
 */
+/*
+
 function areaSelect() {
   var x = document.getElementById("area-select").value;
   document.getElementById("demo").innerHTML += "You selected: ";
@@ -49,3 +101,4 @@ function areaSelect() {
         select.add(el);
       }​
   }
+*/
