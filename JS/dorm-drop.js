39416e2c63504819a8feeb47be8aae935b7170ba
gwenyu517 <gwenyu517@gmail.com>
2019-04-05 12:33:20 -0400

    Still messed up css button. Needs js.

--- JS/dorm-drop.js
+++ JS/dorm-drop.js
@@ -2,27 +2,60 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, []);
 });
+
 const buildings ={
-  beaty:[ 
+  beaty:[
     {val:"beaty", option:"Beaty Towers"}
   ],
-  broward:[ 
+  broward:[
     {val:"broward", option:"Broward Hall"},
     {val:"rawlings", option:"Rawlings Hall"}
   ],
-  murphree:[ 
+  graham:[
+    {val:"graham", option:"Graham Hall"},
+    {val:"simpson", option:"Simpson Hall"},
+    {val:"trusler", option:"Trusler Hall"}
+  ],
+  hume:[
+    {val:"hume", option:"Hume Hall"}
+  ],
+  infinity:[
+    {val:"infinity", option:"Infinity Hall"}
+  ],
+  jennings:[
+    {val:"jennings", option:"Jennings Hall"}
+  ],
+  keys:[
+    {val:"keys", option:"Keys Complex"}
+  ],
+  lakeside:[
+    {val:"lakeside", option:"Lakeside Complex"}
+  ],
+  murphree:[
     {val:"buckman", option:"Buckman Hall"},
     {val:"fletcher", option:"Fletcher Hall"},
     {val:"murphree", option:"Murphree Hall"},
     {val:"sledd", option:"Sledd Hall"},
     {val:"thomas", option:"Thomas Hall"}
   ],
-  yulee:[ 
-    {val:"buckman", option:"Buckman Hall"},
-    {val:"fletcher", option:"Fletcher Hall"},
-    {val:"murphree", option:"Murphree Hall"},
-    {val:"sledd", option:"Sledd Hall"},
-    {val:"thomas", option:"Thomas Hall"}
+  springs:[
+    {val:"springs", option:"Springs Complex"}
+  ],
+  tolbert:[
+    {val:"east", option:"East Hall"},
+    {val:"north", option:"North Hall"},
+    {val:"riker", option:"Riker Hall"},
+    {val:"tolbert", option:"Tolbert Hall"},
+    {val:"weaver", option:"Weaver Hall"}
+  ],
+  yulee:[
+    {val:"cypress", option:"Cypress Hall"},
+    {val:"mallory", option:"Mallory Hall"},
+    {val:"reid", option:"Reid Hall"},
+    {val:"yulee", option:"Yulee Hall"}
+  ],
+  null:[
+    {val:"", option:"Building"}
   ]
 }
 
@@ -40,7 +73,6 @@ const onAreaChange = (val) =>{
     buildings[val].forEach((element)=>{
       console.log(element);
         output += `<option value="${element.val}"  >${element.option}</option>`;
-        
     });
     output += `/select>
         </div>`;
@@ -52,7 +84,35 @@ const onAreaChange = (val) =>{
     $('#building').empty();
   }
 }
-onAreaChange("beaty");
+
+$('#building').on('change', (e) => {
+  onBuildingChange(e.target.value);
+});
+
+const onBuildingChange = (val) =>{
+  if(room[val]){
+    //actually run the rebuild
+    $('#room').empty();
+    let output = `<div class="input-field col s12">
+        <select id="building-select" onchange="">`;
+    buildings[val].forEach((element)=>{
+      console.log(element);
+        output += `<option value="${element.val}"  >${element.option}</option>`;
+    });
+    output += `/select>
+        </div>`;
+    $('#building').append(output);
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, []);
+  }else{
+    //just empty it
+    $('#building').empty();
+  }
+}
+
+
+
+//onAreaChange("null");
 //document.getElementById("area-select").addEventListener("change", areaSelect());
 /*
   var select = document.getElementById("selectNumber");
